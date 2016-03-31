23b662e05f423bff18aae0aae1b54f2847abbd9e
chenqiang <chenqiang@onegohome.com>
2016-03-31 11:26:58 +0800

    rest接口sample
    Change-Id: I405ca94c50a1a7e183bf069e99683ec5ac8da00a

---
+++ src/main/java/com/chenqiang/springrest/domain/Greeting.java
@@ -0,0 +1,24 @@
+package com.chenqiang.springrest.domain;
+
+/**
+ * Created by chenqiang on 16-3-31.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
